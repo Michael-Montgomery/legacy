a6e4d054bad7cbfe0a348e63f6a3795c46d09e81
Michael Montgomery <montgomery.michael@hotmail.com>
2017-01-09 14:59:28 -0500

    init commit

---
+++ public/js/index.js
@@ -0,0 +1,17 @@
+var app = angular.module('legacy', [
+    'ngRoute',
+    'legacy.welcome'
+]);
+
+app.config(function($routeProvider) {
+    $routeProvider.when('/', {
+        redirectTo: '/welcome'
+    }).otherwise({
+        redirectTo: '/welcome'
+    });
+
+    $routeProvider.when('/welcome', {
+        templateUrl: 'public/views/templates/welcome.tpl.html',
+        controller: 'welcomeController'
+    })
+})
---
+++ public/js/welcome.js
@@ -0,0 +1,5 @@
+var app = angular.module('legacy.welcome', []);
+
+app.controller('welcomeController', function($scope) {
+    $scope.message = 'welcome'
+})
