781a93ab29395218174a9113aeb2d263ff4c1133
Michael Montgomery <montgomery.michael@hotmail.com>
2017-01-19 15:19:26 -0500

    homepage styling

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -1,5 +1,11 @@
 var app = angular.module('legacy.welcome', []);
 
 app.controller('welcomeController', function($scope) {
-
+    $(window).scroll(function() {
+        if($(window).scrollTop() == 0) {
+            $('.welcome-header').css('background-color', 'transparent');
+        } else {
+            $('.welcome-header').css('background-color', 'black');
+        }
+    })
 })
