aa4bfa6680564a7d15dbaec7985391b7d4fe41f6
Michael Montgomery <montgomery.michael@hotmail.com>
2017-03-08 11:30:04 -0500

    additional styling for homes page

--- public/js/homes.js
+++ public/js/homes.js
@@ -29,6 +29,35 @@ app.controller('homesController', function($scope) {
             monthlyRate: 850,
             city: 'Melbourne',
             state: 'FL'
+        },
+        {
+            monthlyRate: 800,
+            city: 'Melbourne',
+            state: 'FL'
+        },
+        {
+            monthlyRate: 2000,
+            city: 'Indialantic',
+            state: 'FL'
+        },
+        {
+            monthlyRate: 775,
+            city: 'Palm Bay',
+            state: 'FL'
+        },
+        {
+            monthlyRate: 900,
+            city: 'Eau Gallie',
+            state: 'FL'
+        },
+        {
+            monthlyRate: 850,
+            city: 'Melbourne',
+            state: 'FL'
         }
-    ]
+    ];
+
+    $scope.openTemplate = function(idx) {
+        alert($scope.homeList[idx].monthlyRate)
+    }
 })
