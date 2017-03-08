4365466620f63c41b533f97cdf11ab703aa66d4f
Michael Montgomery <montgomery.michael@hotmail.com>
2017-03-08 10:17:23 -0500

    implementation of ng repeat on homes page

--- public/js/homes.js
+++ public/js/homes.js
@@ -2,5 +2,33 @@ var app = angular.module('legacy.homes', []);
 
 
 app.controller('homesController', function($scope) {
-    $scope.message = 'homes';
+    $scope.homes = {};
+
+    $scope.homeList = [
+        {
+            monthlyRate: 800,
+            city: 'Melbourne',
+            state: 'FL'
+        },
+        {
+            monthlyRate: 2000,
+            city: 'Indialantic',
+            state: 'FL'
+        },
+        {
+            monthlyRate: 775,
+            city: 'Palm Bay',
+            state: 'FL'
+        },
+        {
+            monthlyRate: 900,
+            city: 'Eau Gallie',
+            state: 'FL'
+        },
+        {
+            monthlyRate: 850,
+            city: 'Melbourne',
+            state: 'FL'
+        }
+    ]
 })
